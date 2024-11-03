bf609859bbe07eea8a0de56aa3ba9ceb4691db62
Vovk Oksana <oksvovk85@gmail.com>
2024-11-03 17:36:26 +0200

    Done

---
+++ public/worker.js
@@ -0,0 +1,15 @@
+onmessage = ({ data }) => {
+  console.log(data.data);
+  const fib = (n) => {
+    let a = 0,
+      b = 1;
+    for (let i = 2; i <= n; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return n ? b : a;
+  };
+  const result = fib(data.data);
+  postMessage(result);
+};
--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import "./App.css";
+const worker = new Worker("worker.js");
 
 //Note: please, do dot change placeholder and data-testid attributes
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("Calculating...");
+
+  const handelChange = (e) => {
+    setInputValue(Number(e.currentTarget.value));
+  };
+
+  const onBtnClick = () => {
+    setResult("Calculating...");
+    worker.onmessage = ({ data }) => {
+      setResult(`Result: ${data}`);
+    };
+    worker.postMessage({ data: inputValue });
+
+    setInputValue("");
+  };
+
   return (
     <div className="app">
       <h1>Fibonacci 🌀</h1>
-      <input type="number" placeholder="Insert a number" />
-      <button>Calculate</button>
-      <div className="result" data-testid="result"></div>
+      <input
+        type="number"
+        placeholder="Insert a number"
+        value={inputValue}
+        onChange={handelChange}
+      />
+      <button onClick={onBtnClick}>Calculate</button>
+      <div className="result" data-testid="result">
+        {result}
+      </div>
     </div>
   );
 }
